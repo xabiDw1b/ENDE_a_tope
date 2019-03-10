16032504d82ca3df7279097bd9b9010a32d225e8
Israel de Lamo Blas <i.delamo@gmail.com>
2019-03-10 19:57:31 +0100

    cambios desde visualStudio
    COn un contador desde Visual Studio Codee

--- src/com/ende/pruebasGitHub/HelloWorld.java
+++ src/com/ende/pruebasGitHub/HelloWorld.java
@@ -4,8 +4,13 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		//qu� vacio estoy hoy
-		//aunque tienee solución
+		for (int i = 1; i <=5 ; i++){
+			System.out.println("cuento hasta " + i);
+		}
+		
+		String pedo2 = "Pedo";
+		System.out.println(pedo2);
+
 	}
 
 }
