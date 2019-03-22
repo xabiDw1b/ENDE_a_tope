9ad101ae713afd39e253c96d900bf8bb0ccb95c4
Israel de Lamo <i.delamo@gmail.com>
2019-03-22 13:14:23 +0100

    Crear un nuevo archivo para que todos veamos como se va sincronizando

---
+++ src/com/ende/pruebasGitHub/HolaIsrael.java
@@ -0,0 +1,18 @@
+package com.ende.pruebasGitHub;
+
+/**
+ * Nueva clase de pruebas para GitHyb
+ * @author pei13
+ *
+ */
+
+public class HolaIsrael {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.prinln("Hola, Israel");
+
+	}
+
+}
