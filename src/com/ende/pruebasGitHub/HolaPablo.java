222c8ee93e0962040b964d9e57cf549c6eb3cea7
RedLayer <48572835+RedLayer@users.noreply.github.com>
2019-03-22 14:01:49 +0100

    Create HolaPablo.java
    Hola mundo creado por Pablo

---
+++ src/com/ende/pruebasGitHub/HolaPablo.java
@@ -0,0 +1,17 @@
+package com.ende.pruebasGitHub;
+
+public class HolaPablo {
+	
+	/**
+	 * Clase de prueba de hola mundo para ENDE
+	 * 
+	 * @author Pablo
+	 */
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Pablo");
+
+	}
+
+}
